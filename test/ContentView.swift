adfda919bfea8570e876e7d371dc56c96a5c4161
Roberto Candales <candales.roberto@gmail.com>
2021-10-01 00:36:50 -0300

    initial commit

--- test/ContentView.swift
+++ test/ContentView.swift
@@ -8,9 +8,88 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+    
+    @State var playerCard = "card5"
+    @State var cpuCard = "card9"
+    @State var playerScore = 0
+    @State var cpuScore = 0
+    
+     var body: some View {
+        
+        ZStack {
+       
+            Image("background")
+                .resizable(resizingMode: .stretch)
+                .ignoresSafeArea()
+            
+            VStack {
+                Spacer()
+                Image("logo")
+                Spacer()
+                HStack {
+                    Spacer()
+                    Image(playerCard)
+                    Spacer()
+                    Image(cpuCard)
+                    Spacer()
+                }
+                Spacer()
+                
+                
+                   
+                
+                Button(action: {
+                    
+                    // Generate a random number between 2 and 14
+                    let playerRand = Int.random(in: 2...14)
+                    let cpuRand = Int.random(in: 2...14)
+
+                    //Update the cards
+                    playerCard = "card" + String(playerRand)
+                    cpuCard="card" + String(cpuRand)
+                    
+                    //Update the score
+                    if playerRand > cpuRand {
+                         playerScore += 1
+                        
+                    } else if cpuRand > playerRand {
+                        cpuScore += 1
+                    }
+                    
+                                   
+                }, label: {
+                    Image("dealbutton")
+                })
+                Spacer()
+                HStack {
+                    Spacer()
+                    VStack {
+                        Text("Player")
+                            .font(.headline)
+                            .foregroundColor(Color.white)
+                            .padding(10.0)
+                        Text(String(playerScore)).font(.largeTitle)
+                            .foregroundColor(Color.white)
+                    }
+                    Spacer()
+                    VStack {
+                        Text("CPU")
+                            .font(.subheadline)
+                            .foregroundColor(Color.white).padding(10.0)
+                        Text(String(cpuScore)).font(.largeTitle)
+                            .foregroundColor(Color.white)
+                    }
+                    Spacer()
+                    
+                }
+                Spacer()
+            }
+          
+        }
+      
+        
+        
+        
     }
 }
 
